f05d7b6450870b5319fe2e5d7ff4c496f4119007
Adithya K B <adithyakb93@gmail.com>
2024-08-18 18:17:18 +0530

    feat: Add custom button

--- app/index.tsx
+++ app/index.tsx
@@ -1,17 +1,45 @@
-import { Image, StyleSheet, Platform, View, Text } from "react-native";
+import { ImageBackground, Text, View } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import { LinearGradient } from "expo-linear-gradient";
+import beachImage from "@/assets/meditation-images/beach.webp";
+import { SafeAreaView } from "react-native-safe-area-context";
+import CustomButton from "@/components/CustomButton";
 
-export default function HomeScreen() {
+export default function App() {
   return (
-    <View className="flex-1 justify-center items-center">
-      <Text>Hello World!</Text>
+    <View className="flex-1">
+      <ImageBackground
+        source={beachImage}
+        resizeMode="cover"
+        className="flex-1"
+      >
+        <LinearGradient
+          className="flex-1"
+          colors={["rgba(0,0,0,0.4)", "rgba(0,0,0,0.8)"]}
+        >
+          <SafeAreaView className="flex-1 mx-5 my-12 justify-between">
+            <View>
+              <Text className="text-center text-white font-bold text-4xl">
+                Simple Meditation
+              </Text>
+              <Text className="text-center text-white text-regular text-2xl mt-3">
+                Simplifying Meditation for Everyone
+              </Text>
+            </View>
+
+            <View>
+              <CustomButton
+                onPress={() => {
+                  "tap";
+                }}
+                title="Get Started"
+              />
+            </View>
+
+            <StatusBar style="light" />
+          </SafeAreaView>
+        </LinearGradient>
+      </ImageBackground>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    alignItems: "center",
-    flex: 1,
-    justifyContent: "center",
-  },
-});
